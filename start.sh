5e399583c4e2c4b63475131d959ff01d054e6719
Smit Mehta <smit@coditation.com>
2023-06-12 11:21:34 +0530

    Changes in start.sh

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ pip install  git+https://github.com/IDEA-Research/GroundingDINO.git
 pip install  git+https://github.com/facebookresearch/segment-anything.git
 
 
-python visual_chatgpt.py --load "ImageCaptioning_cuda:0,Text2Image_cuda:0,Text2Image_share:True,Text2Image_ngrok_key:2QvLL6vPZeCSC21Tifizl8e1X9C_55wuEmz9UvoCZYe43Ym5f"
+python visual_chatgpt.py --load "ImageCaptioning_cuda:0,Text2Image_cuda:0"
